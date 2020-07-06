f45d74da46605fb97dbd32d13b993f46ba933796
hmorl <harry.morley@live.co.uk>
2020-07-06 23:47:16 +0100

    Updated script comment

--- open-in-browser.sh
+++ open-in-browser.sh
@@ -1,6 +1,6 @@
 # Kill any instances of python HTTP server
 pkill -f http.server
 
-# Open localhost in Chrome in a new incognito tab after half a second,
-# whilst simulataneously starting up the python HTTP server
+# Open localhost in Chrome in a new incognito tab,
+# and simulataneously start up the python HTTP server
 (open -na "Google Chrome" --args --incognito "http://localhost:8000") & python3 -m http.server
